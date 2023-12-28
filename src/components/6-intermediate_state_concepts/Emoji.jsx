9c2b07ac97657bfdccb2d456fde299cf0721a570
Mauricio Erazo <antonierazo13@hotmail.com>
2023-12-28 18:05:13 +0100

    feat: generate ids with UUID

--- src/components/6-intermediate_state_concepts/Emoji.jsx
+++ src/components/6-intermediate_state_concepts/Emoji.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 const Emoji = () => {
-  const [emojis, setEmojis] = useState(['😇']);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: '😇' }]);
 
   const addEmoji = () => {
-    setEmojis((oldEmojis) => [...oldEmojis, '🥳']);
+    setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: '🤠' }]);
   };
 
   return (
     <div>
       {emojis.map((image) => (
-        <span style={{ fontSize: '3rem' }}>{image}</span>
+        <span key={image.id} style={{ fontSize: '3rem' }}>
+          {image.emoji}
+        </span>
       ))}
       <button onClick={addEmoji}>Add Emoji</button>
     </div>
