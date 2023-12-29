5b6e663207a3b850699580a5d4c9ec5800cc5e4a
Mauricio Erazo <antonierazo13@hotmail.com>
2023-12-29 17:40:24 +0100

    feat: add the logic to win the game, in this case when dice sum 7 and specify in teh app.jsx with the props the number of dice use with the goal we need.
    <LuckyN numDice={3} goal={11} />

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,7 @@ function App() {
       <Emoji />
 
       <LuckyN />
+      <LuckyN numDice={3} goal={11} />
     </>
   );
 }
--- src/components/7-luckyNumbers/LuckyN.jsx
+++ src/components/7-luckyNumbers/LuckyN.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
-import { getRolls } from './utils';
+import { getRolls, sum } from './utils';
 
 import Dice from './Dice';
 
 const LuckyN = ({ numDice = 2, goal = 7 }) => {
   const [dado, setDado] = useState(getRolls(numDice));
 
+  const isWinner = sum(dado) === goal;
+
+  const roll = () => setDado(getRolls(numDice));
+
   return (
     <main className="lucky__component">
-      <h1>Hello Lucky {goal}</h1>
+      <h1>
+        Hello Lucky{goal} {isWinner && 'You won!'}
+      </h1>
       <Dice dice={dado} />
-      <Dice dice={dado} color="brown" />
+      <button onClick={roll}>Re-Roll Dice</button>
     </main>
   );
 };
