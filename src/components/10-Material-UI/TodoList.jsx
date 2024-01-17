843373f7e6bacf9cdf576270dd263ef7e6d66ed4
Mauricio Erazo <antonierazo13@hotmail.com>
2024-01-17 18:09:47 +0100

    feat: make a function to remove todo´s

--- src/components/10-Material-UI/TodoList.jsx
+++ src/components/10-Material-UI/TodoList.jsx
@@ -2,14 +2,7 @@ import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
-
+import TodoItem from './TodoItem';
 
 const initialTodos = [
   { id: uuid(), text: 'Feed the cat', completed: false },
@@ -22,64 +15,20 @@ const initialTodos = [
 // console.log(initialTodos)
 
 export default function TodoList() {
-const [todos, setTodos] = useState(initialTodos)
+  const [todos, setTodos] = useState(initialTodos);
+
+  const removeTodo = (id) =>
+    setTodos((prevTodos) => prevTodos.filter((t) => t.id !== id));
 
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {todos.map( todo => {
-        const labelId = `checkbox-list-label-${todo.id}`;
-
-        return (
-          <ListItem
-            key={todo.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todo.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={todo.text} />
-            </ListItemButton>
-          </ListItem>
-        )
-      })}
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          removeTodo={() => removeTodo(todo.id)}
+        />
+      ))}
     </List>
-  )
+  );
 }
-
-
-// export default function CheckboxList() {
-//   const [checked, setChecked] = React.useState([0]);
-
-//   const handleToggle = (value) => () => {
-//     const currentIndex = checked.indexOf(value);
-//     const newChecked = [...checked];
-
-//     if (currentIndex === -1) {
-//       newChecked.push(value);
-//     } else {
-//       newChecked.splice(currentIndex, 1);
-//     }
-
-//     setChecked(newChecked);
-//   };
-
-//   return (
-//       {[0, 1, 2, 3].map((value) => {
-        
-//         );
-//       })}
-//     </List>
-//   );
-// }
